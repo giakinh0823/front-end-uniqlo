66f24004e6c6474d8d27d6a3644ad3377409ce3f
Gia Kinh <giakinh2000@gmail.com>
2020-12-25 02:08:22 +0700

    done lan

---
+++ js/caurosellist.js
@@ -0,0 +1,40 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+  items: 4,
+  loop: true,
+  margin: 20,
+  responsiveClass: true,
+  URLhashListener:true,
+  lazyLoadEager:true,
+  autoHeight: true,
+  nav: true,
+  autoplay:true,
+  autoplayTimeout:5000,
+    responsive:{
+    0:{
+        items:1
+    },
+    600:{
+        items:2
+    },            
+    960:{
+        items:3
+    },
+    1200:{
+        items:4
+    },
+    scrollPerPage: true,
+    navigation: true
+}
+});
+// owl.on('mousewheel', '.owl-stage', function (e) {
+//   if (e.deltaY > 0) {
+//     owl.trigger('next.owl');
+//   } else {
+//     owl.trigger('prev.owl');
+//   }
+//   e.preventDefault();
+// });
+
+
+
